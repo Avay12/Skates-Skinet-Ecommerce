24b9037dbc22dfd13c5e4881d4dc62ebd0176dff
Avay12 <avaygc@gmail.com>
2024-12-19 15:37:28 +0545

    Http Client & Model Setup

---
+++ Client/src/app/layout/header/header.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { MatIcon } from '@angular/material/icon';
+import { MatButton } from '@angular/material/button';
+import { MatBadge, MatBadgeSize } from '@angular/material/badge';
+@Component({
+  selector: 'app-header',
+  imports: [MatIcon, MatButton, MatBadge],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.scss',
+})
+export class HeaderComponent {}
