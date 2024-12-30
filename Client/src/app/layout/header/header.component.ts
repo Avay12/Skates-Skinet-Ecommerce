2a70956708c30ac8bdd6e462a872a584d21d88c3
Avay12 <avaygc@gmail.com>
2024-12-30 20:38:42 +0545

    Created a volume for docker, Compose up & Reddis Setup

--- Client/src/app/layout/header/header.component.ts
+++ Client/src/app/layout/header/header.component.ts
@@ -1,7 +1,7 @@
 import { Component, inject } from '@angular/core';
 import { MatIcon } from '@angular/material/icon';
 import { MatButton } from '@angular/material/button';
-import { MatBadge, MatBadgeSize } from '@angular/material/badge';
+import { MatBadge } from '@angular/material/badge';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 import { BusyService } from '../../core/services/busy.service';
 import { MatProgressBar } from '@angular/material/progress-bar';
