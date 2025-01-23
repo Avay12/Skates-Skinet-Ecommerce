5641fc08f9ed19769f51703dab2c957e766922a5
Avay12 <avaygc@gmail.com>
2025-01-23 15:21:25 +0545

    Add Stripe integration and update checkout flow

--- Client/src/environments/environment.development.ts
+++ Client/src/environments/environment.development.ts
@@ -1,4 +1,6 @@
 export const environment = {
   production: false,
   apiUrl: 'https://localhost:5001/api/',
+  stripePublicKey:
+    'pk_test_51Qk0YqL76OTeFiIr7Vy9HsbPXiOQyncoEmqjOMPEvkcF2awiZOy2fgcAA5ij0sRGsO3wWCeeSJUWR5KWTxb2EHmZ00CNUGcwG6',
 };
